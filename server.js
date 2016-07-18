3f6c47fd9b3c47dbc925756630bb21edb467aab0
Gabrielle Ong <gabrielleong97@gmail.com>
2016-07-18 17:03:50 +0800

    remove actionhero, add hapi, test routes

--- server.js
+++ server.js
@@ -1,6 +1,41 @@
-// include and initialize the rollbar library with your access token
-var rollbar = require('rollbar')
+'use strict'
+
+// ROLLBAR - include and initialize the rollbar library with your access token
+const rollbar = require('rollbar')
 rollbar.init('cc48181936bf4278a804884acd0ae6a5')
 
-// record a generic message and send to rollbar
-rollbar.reportMessage('Hello world!')
+const Hapi = require('hapi')
+const server = new Hapi.Server()
+
+// Create a server with a host and port
+server.connection({
+  host: 'localhost',
+  port: 3000
+})
+
+// Add the route
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    var data = {
+      key: 'value',
+    }
+    reply(data)
+  }
+})
+server.route({
+  method: 'GET',
+  path: '/hello',
+  handler: function (request, reply) {
+    return reply('hello world')
+  }
+})
+
+// Start the server
+server.start((err) => {
+  if (err) {
+    throw err
+  }
+  console.log('Server running at:', server.info.uri)
+})
---
+++ test/app_tests.js
@@ -0,0 +1,40 @@
+const expect = require('chai').expect
+const supertest = require('supertest')
+const api = supertest('http://localhost:3000')
+
+describe('GET /', (done) => {
+  it("should return a json of 'key: value'", () => {
+    api.get('/')
+      .set('Accept', 'application/json')
+      .expect(200, done)
+  })
+})
+
+describe('GET /hello', (done) => {
+  it("returns 'hello world' at GET /hello", () => {
+    api.get('/hello')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        expect(res.body).to.eq('hello world')
+        done()
+      })
+  })
+})
+
+// describe('GET /candies', function (done) {
+//   it('should return a 200 response', function () {
+//     api.get('/candies')
+//       .set('Accept', 'application/json')
+//       .expect(200, done)
+//   })
+//
+//   it('should return an array', function (done) {
+//     api.get('/candies')
+//       .set('Accept', 'application/json')
+//       .end(function (error, response) {
+//         Candy.find(function (err, candies) {
+//           expect(response.body.length).to.eq(candies.length)
+//           done()
+//         })
+//       })
+//   })
